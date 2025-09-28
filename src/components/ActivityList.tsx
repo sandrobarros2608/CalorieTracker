9008ec8d6fa431b5ad03caa031ebb6b70ac87955
Sandro <sbarroscaballero0826@gmail.com>
2025-09-28 15:15:17 -0500

    Delete Activities

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -2,7 +2,7 @@ import { useMemo } from "react"
 import type { Dispatch } from "react"
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
-import { PencilSquareIcon } from '@heroicons/react/24/outline'
+import { PencilSquareIcon, XCircleIcon } from '@heroicons/react/24/outline'
 import type { ActivityActions } from "../reducers/activity-recuder"
 
 type ActivityListProps = {
@@ -16,6 +16,8 @@ export default function ActivityList({ activities, dispatch } : ActivityListProp
             categories.map(cat => cat.id === category ? cat.name : ''),
         [activities]);
 
+        const isEmptyActivities = useMemo(() => activities.length === 0, [activities]);
+
     return (
         <>
 
@@ -23,7 +25,9 @@ export default function ActivityList({ activities, dispatch } : ActivityListProp
                 Comida y Actividades
             </h2>
 
-            {activities.map(activity => (
+            {isEmptyActivities? <p className="text-center my-5">No hay actividades aún...</p> :
+            
+            activities.map(activity => (
                 <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                     <div className="space-y-2 relative">
                         <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
@@ -42,6 +46,14 @@ export default function ActivityList({ activities, dispatch } : ActivityListProp
                         >
                             <PencilSquareIcon
                                 className="h-8 w-8 text-gray-800"
+                            />
+                        </button>
+
+                        <button
+                            onClick={() => dispatch({type: 'delete-activity', payload: {id: activity.id}})}
+                        >
+                            <XCircleIcon
+                                className="h-8 w-8 text-red-500"
                             />
                         </button>
                     </div>
